eb8265fe21b25b188fc9ed7935b84f0c18db0ea7
Alessandra Castro <140079021+lekadecastro@users.noreply.github.com>
2024-07-23 21:48:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+// 01.
+
+alert("Hello World");
+
+// 02. Soma de dois valores
+
+let numberOne = 10;
+let numberTwo = 10;
+alert(numberOne + numberTwo);
+
+// 03. 
+
+let whatNumber = 50
+
+if (typeof whatNumber === 'number') {
+  alert("É um número");
+} else {
+  alert("Não é um número");
+}
+
+// 04. 
+
+let kindWord = "Property"
+
+if (typeof kindWord === 'string') {
+  alert("É uma string");
+} else {
+  alert("Não é um string");
+}
+
+// 05.
+
+let ifBoolean = true;
+
+if (typeof ifBoolean === 'boolean') {
+  alert("É um booleano");
+} else {
+  alert("Não é um booleano");
+}
+
+// 06.
+
+let numberFour = 250;
+let numberFive = 75;
+alert(numberFour - numberFive);
+
+// 07.
+ 
+let numberSix = 2;
+let numberSeven = 46;
+alert(numberSix * numberSeven);
+
+// 08.
+
+let numberNine = 128;
+let numberTen = 6;
+alert(numberNine / numberTen);
+
+// 09.
+
+let numberEleven = 54;
+
+if (typeof numberEleven === 'number') {
+  if (numberEleven % 2 === 0) {
+    alert("É um número par");
+  } else {
+    alert("Não é um número par");
+  }
+}
+
+// 10.
+
+let numberSixteen = 777;
+
+if (typeof numberSixteen === 'number') {
+  if (numberSixteen % 2 !== 0) {
+    alert("É um número ímpar")
+  } else {
+    alert ("Não é um número ímpar")
+  }
+}
